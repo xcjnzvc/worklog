c2a5354de1f61904e68ec9b1aeb0e9081faca27a
xcjnzvc <rkdenrjd@gmail.com>
2026-07-30 13:46:25 +0900

    fix: Render 배포 환경 포트(PORT) 설정 수정

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -48,7 +48,7 @@ async function bootstrap() {
     },
   });
 
-  // await app.listen(process.env.PORT || 8000);
-  await app.listen(8000, '0.0.0.0');
+  const port = process.env.PORT || 8000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
